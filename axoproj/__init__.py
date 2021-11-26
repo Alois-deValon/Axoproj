249b95fac99794f8c548068ffe635d025943a8f4
Alois-deValon <44165550+Alois-deValon@users.noreply.github.com>
2021-11-26 17:48:00 +0100

    Update __init__.py

--- axoproj/__init__.py
+++ axoproj/__init__.py
@@ -1,1 +1,13 @@
+from axoproj.funcs import (
+  i_near,
+  create_datacube
+)
 
+from axoproj.classes import (
+  Disk_model,
+  WDS_model,
+  DW_model,
+  Infall_model,
+  BS_model,
+  Your_model
+)
